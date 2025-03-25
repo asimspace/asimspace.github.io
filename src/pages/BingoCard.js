6e9f90c65feb772f5626eb216fa1daa33391c75b
asimspace <asim.space@outlook.com>
2025-03-25 13:40:48 -0500

    Remove page Refresh functionality

--- src/pages/BingoCard.js
+++ src/pages/BingoCard.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import Alert from '../components/Alert';
-import { Button, Modal } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+
 
 const BingoCard = () => {
     const [boardNumbers, setBoardNumbers] = useState({});
@@ -10,9 +9,6 @@ const BingoCard = () => {
     const [clickedCells, setClickedCells] = useState({});
     const [tableClass, setTableClass] = useState('');
     const [showAlert, setShowAlert] = useState(true);
-    const [isDirty, setIsDirty] = useState(false); 
-
-    const navigate = useNavigate();
 
     useEffect(() => {
       const columns = {
@@ -31,21 +27,6 @@ const BingoCard = () => {
         generateBingoBoard();
       }
     }, [boardNumbers, clickedCells]);
-
-    useEffect(() => {
-      const handleBeforeUnload = (event) => {
-          if (isDirty) {
-              event.preventDefault();
-              event.returnValue = 'Your current Bingo Card will be lost! Do you want to reload the page?'; // Default browser confirmation dialog
-          }
-      };
-
-      window.addEventListener('beforeunload', handleBeforeUnload);
-
-      return () => {
-          window.removeEventListener('beforeunload', handleBeforeUnload);
-      };
-  }, [isDirty]);
 
     const generateBingoBoard = () => {
       // Create the board rows
@@ -88,7 +69,6 @@ const BingoCard = () => {
     };
 
     const handleCellClick = (cellKey) => {
-      setIsDirty(true);
       setClickedCells(prevState => ({
         ...prevState,
         [cellKey]: !prevState[cellKey]
